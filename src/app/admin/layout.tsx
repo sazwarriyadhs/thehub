deabf491f199d5997385fb4846922b70cd96a19a
Kunyit Asem <kunyitasemmanispedas@gmail.com>
2025-07-10 06:42:24 +0000

    commit

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarNav } from '@/components/sidebar-nav';
 import { fetchAdminUser } from '@/lib/data';
 import AdminLayoutClient from './components/admin-layout-client';
+import AdminLayoutClient from '@/app/admin/components/admin-layout-client';
 
 
 export default async function AdminLayout({
